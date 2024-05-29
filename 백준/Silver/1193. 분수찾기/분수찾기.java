6feef4d7aa3bc62de6d056bb0408174d02cf9d76
lass9436 <57583282+lass9436@users.noreply.github.com>
2024-05-29 13:36:57 +0900

    Update 분수찾기.java

--- 백준/Silver/1193. 분수찾기/분수찾기.java
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -1,6 +1,18 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
+/**
+ * 백준 1193
+ *
+ * 입력 : 숫자
+ * 출력 : 숫자에 해당하는 숫자/숫자 형 출력
+ *
+ * 지그재그로 한 방향으로 이동하는 length 가 1씩 증가함에 초점을 맞췄습니다.
+ * length 를 증가시키면서 boolean up 으로 방향을 관리하고
+ * count 를 1부터 증가시키면서 length 에 도달하면 up을 반전해 방향을 전환했습니다.
+ *
+ * 나머지는 초기값 세팅에만 신경쓰면 될 것 같습니다.
+ */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
