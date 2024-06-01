91c8ee9b946804efe387ddb5083754e20fe5705b
lass9436 <57583282+lass9436@users.noreply.github.com>
2024-06-01 15:30:30 +0900

    [Silver I] Title: IOIOI, Time: 272 ms, Memory: 22068 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -1,44 +1,41 @@
 import java.io.*;
-import java.util.StringTokenizer;
 
 /**
- * BOJ5525 IOIOI
- *
+ * BOJ 5525
+ * 답지 봤습니다.
+ * String 이나 char 를 이용해 비교했지만, O(N^2)으로 실패해서
+ * O(N)의 방법을 찾아야 했습니다.
+ * 최소 IOI 를 찾고 찾는 N 의 IOI...I 가 되면 result++ 하고 
+ * 카운트를 한 개 내리고, i++를 합니다.
  */
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int answer = 0;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
-        String s = br.readLine();
-        int size = 2 * N + 1;
-        int maxIndex = M - size + 1;
+        char s[] = br.readLine().toCharArray();
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < size; i++) {
-            if (i%2 == 0) {
-                sb.append('I');
-            }else{
-                sb.append('O');
+        int result = 0;
+        int count = 0;
+
+        for(int i=1; i < M - 1; i++) {
+            if(s[i - 1] == 'I' && s[i] == 'O' && s[i + 1] == 'I') {
+                count++;
+
+                if(count == N) {
+                    count--;
+                    result++;
+                }
+                i++;
+            }
+            else {
+                count = 0;
             }
         }
-        String ioi = sb.toString();
 
-        for(int i=0;i<maxIndex; i++){
-            String sub = s.substring(i, i + size);
-            if(sub.equals(ioi)){
-                answer++;
-            }
-        }
+        System.out.println(result);
 
-        bw.write(answer + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-}
-
+    } // End of main
+} // End of class
